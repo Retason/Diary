1f1a8473b196eafba6b10fd33fd4c2a1138a93b2
Retason <114427951+Retason@users.noreply.github.com>
2023-02-01 09:35:25 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,199 +1,184 @@
-namespace Ezhednevnik
+using System;
+using System.Collections.Generic;
 
+
+namespace ConsoleDiary
 {
-    internal class Program
 
+    // Хранит массив дней
+    class Diary
     {
-        static void Main(string[] args)
-            
+        public class Day// Хранит массив заметок
         {
-            List<string> dva = new List<string> { "  1. Помыть посуду", "  2. Убрать комнату" };
+            public class Note// Хранит в себе информацию о конкретной заметке
+            {
+                public string name;
+                public string description;
+                public DateTime date;
+                public Note(string name, string description, DateTime date)// Конструктор класса
+                {
+                    this.name = name;
+                    this.description = description;
+                    this.date = date;
+                }
+            }
 
+            public DateTime date;
+            public List<Note> notes = new List<Note>();// Массив для хранения заметок внутри одного дня
 
-            day1();
+            public Day(DateTime date)// Конструктор для создания дня
+            {
+                this.date = date;
+            }
         }
-        
-        
-        static void day1()
+        public List<Day> days = new List<Day>();// Массив для хранения дней внутри ежедневника
 
+        public void addNote(int dayIndex, Day.Note note)// Метод для добавления новой заметки в определенный день. День определяется по индексу
         {
-            
-            List<string> odin = new List<string> { "  1. Приготовить ужин", "  2. Почистить пк" };
-            Console.WriteLine("\tЗаметки 25.05.2022");
-            Console.WriteLine(odin[0]);
-            Console.WriteLine(odin[1]);
-            string F = "Время создания заметки: ";
-
-            int b = 1;
-            Console.SetCursorPosition(0, b);
-            Console.WriteLine("->");
-            ConsoleKeyInfo a = Console.ReadKey();
-
-            while (a.Key != ConsoleKey.Enter)
-
-            {
-                if (a.Key == ConsoleKey.UpArrow)
-
-                {
-                    b--;
-                    Console.Clear();
-                    Console.WriteLine("\tЗаметки 26.05.2022");
-                    Console.WriteLine(odin[0]);
-                    Console.WriteLine(odin[1]);
-                }
-
-                else if (a.Key == ConsoleKey.DownArrow)
-
-                {
-                    b++;
-                    Console.Clear();
-                    Console.WriteLine("\tЗаметки 27.05.2022");
-                    Console.WriteLine(odin[0]);
-                    Console.WriteLine(odin[1]);
-                }
-
-                Console.SetCursorPosition(0, b);
-                Console.WriteLine("->");
-                a = Console.ReadKey();
-
-                if (a.Key == ConsoleKey.RightArrow) { Console.Clear(); day2(); }
-                else if (a.Key == ConsoleKey.LeftArrow) { Console.Clear(); day3(); }
-
-            }
-
-            if (b == 1)
-
-            {
-                Console.Clear();
-                Console.WriteLine(odin[0] + " - Сварить котлетки, пожарить макарошки");
-                Console.WriteLine();
-                Console.WriteLine(F + "25.05.2022 - 04:30:27");
-            }
-
-            else if (b == 2)
-
-            {
-                Console.Clear();
-                Console.WriteLine(odin[1] + " - Разобрать, подуть, собрать");
-                Console.WriteLine();
-                Console.WriteLine(F + "23.05.2022 - 15:13:14");
-            }
-            Console.ReadKey();
-            Console.Clear();
-            day1();
-
-            static void day2()
-            {
-
-                List<string> dva = new List<string> { "  1. Сделать C#", "  2. Пойти в рейтинг" };
-
-                Console.WriteLine("\tЗаметки 07.11.2022");
-                Console.WriteLine(dva[0]);
-                Console.WriteLine(dva[1]);
-                string F = "Время создания заметки: ";
-
-                int b = 1;
-                Console.SetCursorPosition(0, b);
-                Console.WriteLine("->");
-                ConsoleKeyInfo a = Console.ReadKey();
-
-                while (a.Key != ConsoleKey.Enter)
-
-                {
-                    if (a.Key == ConsoleKey.UpArrow)
-
-                    {
-                        b--;
-                        Console.Clear();
-                        Console.WriteLine("\tЗаметки 06.11.2022 - 03:20:32");
-                        Console.WriteLine(dva[0]);
-                        Console.WriteLine(dva[1]);
-                    }
-
-                    else if (a.Key == ConsoleKey.DownArrow)
-
-                    {
-                        b++;
-                        Console.Clear();
-                        Console.WriteLine("\tЗаметки 05.11.2022 - 21:45:39");
-                        Console.WriteLine(dva[0]);
-                        Console.WriteLine(dva[1]);
-                    }
-
-                    Console.SetCursorPosition(0, b);
-                    Console.WriteLine("->");
-                    a = Console.ReadKey();
-                    if (a.Key == ConsoleKey.RightArrow) { Console.Clear(); day3(); }
-                    else if (a.Key == ConsoleKey.LeftArrow) { Console.Clear(); day1(); }
-                }
-
-                if (b == 1)
-                {
-                    Console.Clear();
-                    Console.WriteLine(dva[0] + " - Делать много-много C#, ведь дедлайн близок...");
-                    Console.WriteLine();
-                    Console.WriteLine(F + " 02.11.2022 - 14:33:41");
-                }
-                else if (b == 2)
-                {
-                    Console.Clear();
-                    Console.WriteLine(dva[1] + " - Зайти в дс, пойти в катку, словить лузстрик, вспомнить про C#");
-                    Console.WriteLine();
-                    Console.WriteLine(F + " 02.11.2022 - 18:32:12");
-                }
-                Console.ReadKey();
-                Console.Clear();
-                day2();
-            }
-
-            static void day3()
-            {
-                List<string> tri = new List<string> { "  1. Съездить в тц" };
-                Console.WriteLine("\tЗаметки 21.05.2022");
-                Console.WriteLine(tri[0]);
-                string F = "Время создания заметки: ";
-
-                int b = 1;
-                Console.SetCursorPosition(0, b);
-                Console.WriteLine("->");
-                ConsoleKeyInfo a = Console.ReadKey();
-
-                while (a.Key != ConsoleKey.Enter)
-                {
-                    if (a.Key == ConsoleKey.UpArrow)
-                    {
-                        b--;
-                        Console.Clear();
-                        Console.WriteLine("\tЗаметки 21.05.2022");
-                        Console.WriteLine(tri[0]);
-
-                    }
-                    else if (a.Key == ConsoleKey.DownArrow)
-                    {
-                        
-                        b++;
-                        Console.Clear();
-                        Console.WriteLine("\tЗаметки 21.05.2022");
-                        Console.WriteLine(tri[0]);
-
-                    }
-                    Console.SetCursorPosition(0, b);
-                    Console.WriteLine("->");
-                    a = Console.ReadKey();
-
-                    if (a.Key == ConsoleKey.RightArrow) { Console.Clear(); Console.WriteLine("Ваши заметки кончились, переход к первой заметке"); day1(); }
-                    else if (a.Key == ConsoleKey.LeftArrow) { Console.Clear(); day2(); }
-                }
-
-                Console.Clear();
-                Console.WriteLine(tri[0] + "  - Приехать в тц, погулять по тц, пойти в мак");
-                Console.WriteLine();
-                Console.WriteLine(F + " 10.10.2022 - 21:12:32");
-
-            }
-
+            days[dayIndex].notes.Add(note);
         }
-
     }
 
+    class userInterface// Класс для отрисовки интерфейса
+    {
+
+        int arrowPos = 0;// Позиция стрелки в текущий момент
+        Diary diary;// Хранит ежедневник, который будет отрисовываться
+        int dayIndex = 0;// Отслеживает какой день отрисовывать
+        private void ShowDay(Diary.Day day)// Метод для отрисовки дня
+        {
+            Console.Clear();// Очищаем консоль
+            Console.WriteLine("День: {0}", day.date.ToShortDateString());
+            for (int j = 0; j < day.notes.Count; j++)// Цикл по всем доступным заметкам этого дня
+            {
+                if (arrowPos == j)// Если позиция стрелки находится напротив этой заметки - её отрисовываем
+                {
+                    Console.WriteLine("->" + day.notes[j].name);
+                }
+                else// Если нет-оставляем место пустым
+                {
+                    Console.WriteLine("  " + day.notes[j].name);
+                }
+            }
+            //Console.WriteLine(arrowPos);
+        }
+
+        private void ShowNote(Diary.Day.Note note)
+        {
+            Console.Clear();
+            Console.WriteLine("День: {0}", note.date.ToShortDateString());
+            Console.WriteLine("Название: " + note.name);
+            Console.WriteLine("Описание: " + note.description);
+
+        }
+
+        private void arrowInc()
+        {
+            if (arrowPos + 1 < diary.days[dayIndex].notes.Count)
+                arrowPos++;
+            else
+                arrowPos = 0;
+        }// Инкремент для позиции стрелки, благодаря ему она не выйдет за границы диапазона. Далее аналогично
+
+        private void arrowDec()
+        {
+            if (arrowPos - 1 >= 0)
+                arrowPos--;
+            else
+                arrowPos = diary.days[dayIndex].notes.Count - 1;
+        }
+
+        private void dayIndexInc()
+        {
+            if (dayIndex + 1 < diary.days.Count)
+                dayIndex++;
+            else
+                dayIndex = 0;
+        }
+
+        private void dayIndexDec()
+        {
+            if (dayIndex - 1 >= 0)
+                dayIndex--;
+            else
+                dayIndex = diary.days.Count - 1;
+        }
+
+
+        public void Work()// Главный метод, который запускает работу приложения
+        {
+            ShowDay(diary.days[dayIndex]);// Первая отрисовка дня
+            while (true)
+            {
+
+                ConsoleKeyInfo keyPushed = Console.ReadKey();// Считываем какую кнопку пользователь нажал
+                switch (keyPushed.Key)// В зависимости от нажатой кнопки выполняются разные наборы функций
+                {
+                    case ConsoleKey.DownArrow:
+                        arrowInc();
+                        ShowDay(diary.days[dayIndex]);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        arrowDec();
+                        ShowDay(diary.days[dayIndex]);
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        dayIndexInc();
+                        ShowDay(diary.days[dayIndex]);
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        dayIndexDec();
+                        ShowDay(diary.days[dayIndex]);
+                        break;
+                    case ConsoleKey.Enter:
+                        ShowNote(diary.days[dayIndex].notes[arrowPos]);
+                        break;
+                    case ConsoleKey.Escape:
+                        ShowDay(diary.days[dayIndex]);
+                        arrowPos = 0;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+        public userInterface(Diary diary)
+        {
+            this.diary = diary;
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Init(Diary diary)// Инициализация ежедневника 3мя днями и 7ю заметками
+        {
+            DateTime date1 = DateTime.Now;
+            diary.days.Add(new Diary.Day(date1));
+            diary.addNote(0, new Diary.Day.Note("Пойти в гараж", "Не забыть инструменты", date1));
+            diary.addNote(0, new Diary.Day.Note("Пойти за удочками", "Не забыть деньги", date1));
+
+            DateTime date2 = DateTime.Now.AddDays(5);
+            diary.days.Add(new Diary.Day(date2));
+            diary.addNote(1, new Diary.Day.Note("Выкинуть мусор", "Кошка - не мусор", date2));
+            diary.addNote(1, new Diary.Day.Note("Построить дом", "В конце порадовать себя шоколадкой", date2));
+
+            DateTime date3 = DateTime.Now.AddDays(10);
+            diary.days.Add(new Diary.Day(date3));
+            diary.addNote(2, new Diary.Day.Note("Посадить дерево", "Но не дуб", date3));
+            diary.addNote(2, new Diary.Day.Note("Вырастить сына", "Играть в его радиоуправляемый вертолётик", date3));
+            diary.addNote(2, new Diary.Day.Note("Вырастить дочь", "Не давать красить ногти", date3));
+        }
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();// Создаём ежедневник
+            Init(diary);// Инициализируем
+
+            userInterface userInterface = new userInterface(diary);// Создаём интерфейс, передаём туда ежедневник
+            userInterface.Work();// Запускаем
+        }
+    }
 }
